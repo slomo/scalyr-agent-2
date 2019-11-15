e9d3ca07422d20c9e537c87163a470bb4635a172
Arthur Kamalov <artur.kamalov@akvelon.com>
2019-11-15 12:16:45 +0300

    Add coverage. Stopping by coverage kill. Ad -it

--- .circleci/smoketest_k8s.sh
+++ .circleci/smoketest_k8s.sh
@@ -150,5 +150,5 @@ ${contname_verifier} ${max_wait} \
 
 
 echo "Stopping agent"
-kubectl exec ${agent_hostname} killall coverage
+kubectl exec -it ${agent_hostname} killall coverage
 kubectl cp ${agent_hostname}:/.coverage .
